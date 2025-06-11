60587652ac4c7fe83f44dd8f5dc886bf6405b42b
andersonheidrich <andersonheidrich@hotmail.com>
2025-06-11 16:38:57 -0300

    ajustada a responsividade do Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,32 +1,67 @@
 import { useNavigate } from "react-router-dom";
 import * as Style from "./Header.styles";
+import { useState } from "react";
+import LogoIMG from "../../images/logo.png";
 
 const Header = () => {
+  const [showSandwich, setShowSandwich] = useState(false);
+
+  const toggleSandwich = () => {
+    setShowSandwich(!showSandwich);
+  };
+
   const navigate = useNavigate();
 
   return (
     <Style.Container>
       <Style.Navigation>
-        <Style.Menu>
-          <div className="menu-item">
-            <span onClick={() => navigate("/")}>Início</span>
+        <Style.Logo>
+          <img src={LogoIMG} onClick={() => navigate("/")} />
+        </Style.Logo>
+        <Style.Menu className={showSandwich ? "show" : ""}>
+          <div className="menu-sandwich" onClick={toggleSandwich}>
+            <div className="brand" />
+            <div className="brand" />
+            <div className="brand" />
           </div>
-          <div className="menu-item">
-            <span onClick={() => navigate("/sorteio")}>Sorteio</span>
+          <div className="nav-menu">
+            <div
+              className="menu-item"
+              onClick={() => {
+                setShowSandwich(!showSandwich);
+                navigate("/");
+              }}
+            >
+              <span>Início</span>
+            </div>
+            <div
+              className="menu-item"
+              onClick={() => {
+                setShowSandwich(!showSandwich);
+                navigate("/sorteio");
+              }}
+            >
+              <span>Sorteio</span>
+            </div>
+            <div
+              className="menu-item"
+              onClick={() => {
+                setShowSandwich(!showSandwich);
+                navigate("/login");
+              }}
+            >
+              <span>Login</span>
+            </div>
+            <div
+              className="menu-item"
+              onClick={() => {
+                setShowSandwich(!showSandwich);
+                navigate("/perfil");
+              }}
+            >
+              <span>Meu Perfil</span>
+            </div>
           </div>
-          <div className="menu-item">
-            <span onClick={() => navigate("/login")}>Login</span>
-          </div>
-          <div className="menu-item">
-            <span onClick={() => navigate("/perfil")}>Meu Perfil</span>
-          </div>
-          {/* <div className="menu-item">
-            {isLoggedIn ? (
-              <span onClick={handleLogout}>Logout</span>
-            ) : (
-              <span onClick={handleLogin}>Login</span>
-            )}
-          </div> */}
         </Style.Menu>
       </Style.Navigation>
     </Style.Container>
--- src/components/Header/Header.styles.jsx
+++ src/components/Header/Header.styles.jsx
@@ -7,33 +7,26 @@ export const Container = styled.div`
   width: 100%;
   justify-content: center;
   align-items: center;
-  /* background-color: #ff8c00; */
   font-family: Barlow, sans-serif;
   text-shadow: 1px 1px 3px rgba(0, 0, 0, 0.4);
 
-  background-color: #000000;
-
   span {
-    margin-top: 4px;
     font-size: 20px;
     font-weight: bold;
-    color: #f1c615;
-    /* color: #800080; */
-    /* color: #3b0a59; */
-    cursor: pointer;
 
-    &:hover {
-      /* color: #505050; */
-      /* color: #6c14a3; */
-      /* color: #b100b1; */
-      color: #ed6104;
+    @media screen and (max-width: 990px) {
+      font-size: 18px;
+    }
+
+    @media screen and (max-width: 425px) {
+      font-size: 16px;
     }
   }
 `;
 
 export const Navigation = styled.div`
   display: flex;
-  width: 86%;
+  width: 94%;
   justify-content: center;
   align-items: center;
 `;
@@ -42,18 +35,110 @@ export const Home = styled.div`
   width: 180px;
 `;
 
+export const Logo = styled.div`
+  margin-top: auto;
+  margin-bottom: auto;
+
+  img {
+    width: 80px;
+    max-width: 80px;
+    cursor: pointer;
+
+    @media screen and (max-width: 990px) {
+      width: 70px;
+    }
+
+    @media screen and (max-width: 480px) {
+      width: 60px;
+    }
+  }
+`;
+
 export const Menu = styled.div`
   display: flex;
   width: 100%;
   justify-content: center;
   align-items: center;
 
+  .nav-menu {
+    display: flex;
+
+    @media screen and (max-width: 768px) {
+      flex-direction: column;
+      width: 100%;
+    }
+  }
+
   .menu-item {
     display: flex;
     flex-direction: column;
     justify-content: center;
     align-items: center;
-    width: 140px;
-    margin: 0 8px 0 8px;
+    width: 120px;
+    margin: 0 8px;
+    cursor: pointer;
+    border: 1px solid black;
+    border-radius: 4px;
+    padding: 4px;
+
+    &:hover {
+      color: #ed6104;
+    }
+  }
+
+  .menu-sandwich {
+    display: none;
+  }
+
+  @media screen and (max-width: 990px) {
+    .menu-item {
+      width: 100px;
+    }
+  }
+
+  @media screen and (max-width: 768px) {
+    flex-direction: column;
+    align-items: flex-end;
+
+    .menu-item {
+      display: none;
+    }
+
+    &.show {
+      max-height: 100%;
+      flex-direction: column;
+      align-items: flex-end;
+      background-color: #f5eedc;
+      box-shadow: 1px 1px 3px rgba(128, 128, 128, 0.15);
+      position: absolute;
+      top: 0px;
+      right: 0;
+      width: 100%;
+      padding: 10px;
+
+      .menu-item {
+        display: flex;
+        flex-direction: row;
+        justify-content: flex-start;
+        margin-bottom: 4px;
+      }
+    }
+
+    .menu-sandwich {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: space-evenly;
+      width: 20px;
+      height: 20px;
+      cursor: pointer;
+      margin-right: 16px;
+    }
+
+    .brand {
+      width: 100%;
+      height: 3px;
+      background-color: black;
+    }
   }
 `;
